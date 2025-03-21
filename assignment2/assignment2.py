91569bab97d8d5384acf61c55442b5113ca4974c
Reza Zahedi <r.zahedi@gmail.com>
2025-03-21 12:47:32 -0700

    Task 6: Find the Employee with a Lambda

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -44,3 +44,7 @@ def employee_find(employee_id ):
 
   matches=list(filter(employee_match, employees["rows"]))
   return matches
+
+def employee_find_2(employee_id):
+  matches = list(filter(lambda row : int(row[employee_id_column]) == employee_id , employees["rows"]))
+  return matches
