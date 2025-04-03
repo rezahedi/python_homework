3779c8f526b232b70c20ec4a380f3413cd6d898e
Reza Zahedi <r.zahedi@gmail.com>
2025-04-02 18:38:50 -0700

    Task 4: Data Cleaning

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -51,3 +51,37 @@ print(employee_shape)
 
 # %% 4. Use the info() method
 print(more_employees.info())
+
+# Task 4: Data Cleaning
+# %% 1. Create a DataFrame from dirty_data.csv file and assign it to the variable dirty_data.
+dirty_data = pd.read_csv('dirty_data.csv')
+print(dirty_data)
+clean_data = dirty_data.copy()
+
+# %% 2. Remove any duplicate rows from the DataFrame
+clean_data.drop_duplicates(inplace=True)
+print(clean_data)
+
+# %% 3. Convert Age to numeric and handle missing values
+clean_data['Age'] = pd.to_numeric(clean_data['Age'], errors='coerce')
+print(clean_data)
+
+# %% 4. Convert Salary to numeric and replace known placeholders (unknown, n/a) with NaN
+clean_data['Salary'] = pd.to_numeric(clean_data['Salary'], errors='coerce')
+print(clean_data)
+
+# %% 5. Fill missing numeric values (use fillna).  Fill Age which the mean and Salary with the median
+clean_data['Age'] = clean_data['Age'].fillna(clean_data['Age'].mean())
+clean_data['Salary'] = clean_data['Salary'].fillna(clean_data['Salary'].median())
+print(clean_data)
+
+# %% 6. Convert Hire Date to datetime
+clean_data['Hire Date'] = pd.to_datetime(clean_data['Hire Date'], errors='coerce')
+# There wasn't any instruction on how to handle NaT values, so I filled it with the oldest date in the dataframe to pass the test case, as it return errors on NaT values.
+clean_data['Hire Date'] = clean_data['Hire Date'].fillna(clean_data['Hire Date'].min())
+print(clean_data)
+
+# %% 7. Strip extra whitespace and standardize Name and Department as uppercase
+clean_data['Name'] = clean_data['Name'].str.strip().str.capitalize()
+clean_data['Department'] = clean_data['Department'].str.strip().str.upper()  # Standardize Department to uppercase
+print(clean_data)
